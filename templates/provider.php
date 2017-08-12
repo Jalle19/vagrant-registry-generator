9b39b23b40c072f6b4289b482dac9e675a011a4a
Sam Stenvall <sam.stenvall@nordsoftware.com>
2017-08-12 17:30:01 +0300

    Use a table when rendering the list of providers

--- templates/provider.php
+++ templates/provider.php
@@ -4,21 +4,12 @@ use Jalle19\VagrantRegistryGenerator\Registry\Manifest\Provider;
 
 /* @var Provider $provider */
 
+$metadata = $provider->getFileMetadata();
+
 ?>
-<div class="provider">
-    <?php
-
-    if ($provider->hasMetadata()) {
-        $metadata = $provider->getFileMetadata();
-
-        ?>
-        <div class="fileMetadata">
-            <span class="fileName"><?php echo $metadata->getFileName(); ?></span>
-            <span class="fileSize"><?php echo (int)($metadata->getSize() / 1024 / 1024); ?> MiB</span>
-            <span class="lastModified"><?php echo $metadata->getTimestamp()->format('c'); ?></span>
-        </div>
-        <?php
-    }
-
-    ?>
-</div>
+<tr>
+    <td><?php echo $metadata->getFileName(); ?></td>
+    <td><?php echo (int)($metadata->getSize() / 1024 / 1024); ?> MiB</td>
+    <td><?php echo $metadata->getTimestamp()->format('c'); ?></td>
+</tr>
+<?php
--- templates/version.php
+++ templates/version.php
@@ -11,15 +11,27 @@ use Jalle19\VagrantRegistryGenerator\Registry\Manifest\Version;
     <a href="#<?php echo $version->getVersion(); ?>" name="<?php echo $version->getVersion(); ?>">
         <h2 class="header"><?php echo $version->getVersion(); ?></h2>
     </a>
-    <?php
+    <table>
+        <tr>
+            <th>Filename:</th>
+            <th>Size:</th>
+            <th>Last modified:</th>
+        </tr>
+        <?php
 
-    foreach ($version->getProviders() as $provider) {
-        $this->insert('provider', [
-            'provider'    => $provider,
-        ]);
-    }
+        foreach ($version->getProviders() as $provider) {
+            // Skip providers without metadata
+            if (!$provider->hasMetadata()) {
+                continue;
+            }
+            
+            $this->insert('provider', [
+                'provider' => $provider,
+            ]);
+        }
 
-    ?>
+        ?>
+    </table>
     <pre class="vagrantFile"><?php echo trim($this->fetch('vagrantFile', [
             'boxName'     => $boxName,
             'manifestUrl' => $manifestUrl,
